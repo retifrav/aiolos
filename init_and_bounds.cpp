retifrav/aiolos

commit 17b3166413eeb76a7c2363aaa1d59333d1d10c67

Bugfixes in coll heat transfer and cooling functions. QoL functions and index lookup functions split.

init_and_bounds.cpp
@@ -96,6 +96,7 @@ c_Sim::c_Sim(string filename_solo, string speciesfile_solo, string workingdir, i
         radiation_matter_equilibrium_test = read_parameter_from_file<int>(filename,"RAD_MATTER_EQUI_TEST", debug, 0).value; //Unused/only for tests
         radiation_diffusion_test_linear   = read_parameter_from_file<int>(filename,"RAD_DIFF_TEST_LIN", debug, 0).value;    //Unused/only for tests
         radiation_diffusion_test_nonlinear= read_parameter_from_file<int>(filename,"RAD_DIFF_TEST_NLIN", debug, 0).value;   //Unused/only for tests
+        couple_J_into_T = read_parameter_from_file<int>(filename,"COUPLE_J_INTO_T", debug, 1).value;   //Unused/only for tests
         
         if(debug > 0) cout<<"Using integration order "<<order<<" while second order would be "<<IntegrationType::second_order<<endl;
         
@@ -209,6 +210,7 @@ c_Sim::c_Sim(string filename_solo, string speciesfile_solo, string workingdir, i
         temperature_model = read_parameter_from_file<char>(filename,"INIT_TEMPERATURE_MODEL", debug, 'P').value; //Initialize temperature as adiabatic+constant (P) or constant (C)
         friction_solver   = read_parameter_from_file<int>(filename,"FRICTION_SOLVER", debug, 0).value; //0: no friction, 1: analytic (only for two species), 2: numerical
         do_hydrodynamics  = read_parameter_from_file<int>(filename,"DO_HYDRO", debug, 1).value;  //Switch hydrodynamics, including its CFL condition on/off
+        start_hydro_time  = read_parameter_from_file<double>(filename,"START_HYDRO_TIME", debug, -1.).value; //Hydro is started once globalTime > start_hydro_time and do_hydro == 1
         photochemistry_level = read_parameter_from_file<int>(filename,"PHOTOCHEM_LEVEL", debug, 0).value; //1==C2Ray solver, 2==general photo and thermochemistry solver
         dust_to_gas_ratio = read_parameter_from_file<double>(filename,"DUST_TO_GAS", debug, 0.).value;    //dust-to-gas ratio for Semenov/Malygin opacities
         temperature_floor = read_parameter_from_file<double>(filename,"TEMPERATURE_FLOOR", debug, 0.).value;  //Limit the temperature to a minimum in radiation solver (negative T crashes can still occur due to negative pressures, whichever is found first)
@@ -237,7 +239,7 @@ c_Sim::c_Sim(string filename_solo, string speciesfile_solo, string workingdir, i
         rad_solver_max_iter = read_parameter_from_file<int>(filename,"MAX_RAD_ITER", debug, 1).value;  //Currently unused
         xi_rad              = read_parameter_from_file<double>(filename,"XI_RAD", debug, 2.).value;    //Radiation Xi factor, see Fig. 5 in code paper.
         bond_albedo       = read_parameter_from_file<double>(filename,"BOND_ALBEDO", debug, 0.).value; //Bond albedo, Number between 0. and 1.
-        
+        use_init_discont_smoothing = read_parameter_from_file<int>(filename,"INIT_DISCONT_SMOOTHING", debug, 0).value;  //Smooths out discontinuities in the initial density profiles with a powerlaw. Applied before init_wind discontinuity is put in.
         
         if(problem_number == 2)
             monitor_output_index = num_cells/2; 
@@ -647,6 +649,9 @@ c_Sim::c_Sim(string filename_solo, string speciesfile_solo, string workingdir, i
             }
         }
         
+        //Finally, look for the species indices for some important species. Stored in the utility integers e_idx, hnull_idx, hplus_idx etc.
+        init_highenergy_cooling_indices();
+        
         ///////////////////////////////////////////////////////////////////////// 
         /////////////////////////////////////////////////////////////////////////
         /////////////////////////////////////////////////////////////////////////
@@ -781,7 +786,7 @@ c_Sim::c_Sim(string filename_solo, string speciesfile_solo, string workingdir, i
     
     double templumi = 0;
     
-    if(fluxfile.compare("---")==0) {
+    if(true) {
         
             for(int b=0; b<num_bands_in; b++) {
                 if(debug > 0) cout<<"Init: Assigning stellar luminosities. b ="<<b<<endl;
@@ -826,7 +831,9 @@ c_Sim::c_Sim(string filename_solo, string speciesfile_solo, string workingdir, i
                 
         }
         
-    } else {
+    } 
+    
+    if(fluxfile.compare("---")!=0) {//else {
         
         for(int b = 0; b<num_bands_in; b++) {
             
@@ -858,9 +865,9 @@ c_Sim::c_Sim(string filename_solo, string speciesfile_solo, string workingdir, i
              
              if(cnt > 0) {
                  tempenergy /= (double)cnt;
-                 solar_heating(b) = tempenergy * fluxmultiplier;
+                 solar_heating(b) += tempenergy * fluxmultiplier;
                  templumi += solar_heating(b);
-                 solar_heating_final(b) = solar_heating(b);
+                 solar_heating_final(b) += solar_heating(b);
             } else {
                 solar_heating(b) = 0.;
             }
@@ -1470,14 +1477,7 @@ void c_Species::initialize_hydrostatic_atmosphere(string filename) {
             //if(speciesname.compare("e-")==0 ) { //Force light electrons to be the same number as protons
                 
                 //int p_index = base->get_species_index("H+");
-                int p_index = base->get_species_index("S1");
-                if(p_index == -1)
-                    p_index = base->get_species_index("H+");
-                if(p_index == -1)
-                    p_index = base->get_species_index("p+");
-                if(p_index == -1)
-                    p_index = base->get_species_index("p");
-                
+                int p_index = base->get_species_index("S1 H+ p+ p", 0);
                 if(p_index < 0) {
                     cout<<" IN HYDROSTAT CONSTRUCTION for e-: Protons (H+) species not found! "<<endl;
                 }
@@ -1488,14 +1488,33 @@ void c_Species::initialize_hydrostatic_atmosphere(string filename) {
             //////////////////////////////////////////////////////////////////////////////////////
             //double floor = base->density_floor / mass_amu * std::pow(base->x_i12[i]/base->x_i12[1], -4.);
             
-            double floor = base->density_floor * std::pow(base->x_i12[i]/base->x_i12[1], -4.) * initial_fraction;
-            if( (temp_rhofinal < floor) || ( base->x_i12[i] > 0.5*base->rhill  && base->use_tides == 2       ) ) {
+            double floor          = base->density_floor * std::pow(base->x_i12[i]/base->x_i12[1], -4.) * initial_fraction;
+            //double lastval_scaled = u[i].u1 * std::pow(base->x_i12[i]/base->x_i12[i+1], 8.);
+            double lastval_scaled = u[i].u1 * u[i].u1/u[i-1].u1;
+            
+            if(temp_rhofinal < 0.) {
+                
+                if(lastval_scaled > floor && base->use_init_discont_smoothing)
+                    temp_rhofinal = lastval_scaled;
+                else
+                    temp_rhofinal = lastval_scaled; //this was formerly triggered when  //if( (temp_rhofinal < floor) || ( base->x_i12[i] > 0.5*base->rhill  && base->use_tides == 2       ) ) {
+                
+                negdens = 1;
+            }
+            /*
+            if(base->use_init_discont_smoothing && temp_rhofinal < 0. && temp_rhofinal > floor) {
+                //if(temp_rhofinal < lastval_scaled)
+                    temp_rhofinal = lastval_scaled;
+            }
+            
+            //if( (temp_rhofinal < floor) || ( base->x_i12[i] > 0.5*base->rhill  && base->use_tides == 2       ) ) {
+            if(false){
                 
                 if(temp_rhofinal < 0.)
                     negdens = 1;
                 
                 temp_rhofinal = floor;
-            }
+            }*/
             //////////////////////////////////////////////////////////////////////////////////////
             
             //if(this_species_index == 2) {
@@ -1813,7 +1832,19 @@ void c_Species::apply_boundary_left(std::vector<AOS>& u) {
                 else {
                     dens_wall = BACKGROUND_U.u1 *  mass_amu;
                 }*/
-                eos->compute_primitive(&u[i],&prim[i], 1) ; //Ghost cell u is fixed after init, only need to update p in prim
+                double dens_wall;
+                AOS_prim prim;
+                if(base->problem_number == 1)
+                    dens_wall = SHOCK_TUBE_UL.u1;
+                else {
+                    prim.density = BACKGROUND_U.u1;
+                    prim.speed   = this->prim[2].speed;
+                    prim.temperature = const_T_space;
+                }
+                
+                eos->update_eint_from_T(&(prim), 1);
+                eos->update_p_from_eint(&(prim), 1);
+                eos->compute_conserved(&(prim), &(u[i]), 1) ; //Ghost cell u is fixed after init, only need to update p in prim
             }
             
             break;

advection.cpp
@@ -59,9 +59,10 @@ void c_Sim::execute() {
     ////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~////
     for (globalTime = 0; (globalTime < t_max) && (steps < maxsteps); ) {
         
-//          if(globalTime > 1e3) {   //Comment in if a radiative equilibrium phase is desired before starting hydro
-//              do_hydrodynamics = 1;     
-//          }
+    
+          if(start_hydro_time > 0. && globalTime > start_hydro_time) {   //Comment in if a radiative equilibrium phase is desired before starting hydro
+              do_hydrodynamics = 1;     
+          }
 
         if(steps==0) {
             for(int s = 0; s < num_species; s++)
@@ -234,8 +235,10 @@ void c_Sim::execute() {
                     if(steps % dt_skip_ichem == 0) {
                         dt_skip_dchem += dt;
                         reset_dS();
+                        
                         //cout<<" Running chemistry with dt/dt_chem/dt_skip/steps = "<<dt<<" / "<<dt_skip_dchem<<" / "<<dt_skip_ichem<<" / "<<steps<<endl;
                         do_chemistry(dt_skip_dchem);
+                        
                         dt_skip_dchem = 0.;
                     } else {
                         dt_skip_dchem += dt;
@@ -244,7 +247,9 @@ void c_Sim::execute() {
                     
             }
             
+             
             update_dS();               //Compute low-energy dS
+        
             
             if(false) {
                 cout<<"Pos 3 dS_UV = "<<dS_band(num_cells-10,0)<<endl;
@@ -254,6 +259,7 @@ void c_Sim::execute() {
                 update_fluxes_FLD();   //FLD Radiation transport, updating Temperatures and photon band energies
             }
             else if (use_rad_fluxes == 2){
+                   
                 update_fluxes_FLD_simple(dt); //'Simple' FLD solver
                 
             }

aiolos.h
@@ -385,6 +385,7 @@ public:
     int use_drag_predictor_step;
     int use_convective_fluxes;
     double convect_boundary_strength;
+    double start_hydro_time;
     
     ////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     //
@@ -478,6 +479,14 @@ public:
     ////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     
     double right_extrap_press_multiplier;
+    
+    ////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    //
+    // Initial conditions
+    //
+    ////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    int use_init_discont_smoothing;
     
     ////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     //
@@ -582,12 +591,25 @@ public:
     double init_radiation_factor;
     double radiation_rampup_time;
     
+    //Indices for highenergy cooling
+    int hnull_idx; 
+    int hplus_idx;
+    int e_idx;
+    int C_idx;
+    int Cp_idx;
+    int Cpp_idx;
+    int O_idx;
+    int Op_idx;
+    int Opp_idx;
+    int h3plus_idx;
+    
     //int radiation_solver;
     int use_planetary_temperature;
     int closed_radiative_boundaries ;
     int radiation_matter_equilibrium_test; //If set to 1, sets J = J_init in update_radiation()
     int radiation_diffusion_test_linear;
     int radiation_diffusion_test_nonlinear;
+    int couple_J_into_T;
     double no_rad_trans;      // Multiplier for the div F radiation transport in the radiation solver to compare to models which don't cool thermally
     double CFL_break_time; //Numerical time after which cflfactor=0.9. Used in get_cfl_timestep()
     double photocooling_multiplier;
@@ -721,7 +743,7 @@ public:
     void write_into_execution_log(string dir, string par, string spcfile);
     
     void compute_total_pressure();
-    int get_species_index(const string name);
+    int get_species_index(const string name, const int verbose);
     
     //
     // Friction
@@ -760,6 +782,7 @@ public:
     void update_opacities();
     
     void do_photochemistry();
+    void init_highenergy_cooling_indices();
     
     void update_fluxes(double timestep);           //  Called from transport_radiation#   
     void update_fluxes_FLD();           //  Called from transport_radiation#

commit 89f702044a6c4a0b06683dde823c6370e4a35278

Fix zero-density left ghost cells in fixed boundary (regression from 17b3166)
apply_boundary_left's BoundaryType::fixed branch computed dens_wall for
problem_number == 1 but never wrote it into the zero-initialized AOS_prim, so
compute_conserved zeroed the ghost cells instead of filling them with the left
state. u2/u1 then produced NaN temperatures, the soft-crash detector fired on
step 1, and every shock tube wrote a t-1 "crash dump" that still held the
initial condition -- which is why the Riemann L1 norms came out ~10x over
target rather than obviously broken.

17b3166 replaced a compute_primitive call that left the init-fixed ghost u[i]
alone with code that overwrites it. apply_boundary_right was never affected and
still assigns SHOCK_TUBE_UR directly, so mirror that: SHOCK_TUBE_UL is already
the conserved left state built by the same eos at init.

Restores check.shock_tube (all seven Riemann problems, 13-53% of the L1
budget) and check.dustyshock.

init_and_bounds.cpp
@@ -1850,22 +1850,27 @@ void c_Species::apply_boundary_left(std::vector<AOS>& u) {
                 else {
                     dens_wall = BACKGROUND_U.u1 *  mass_amu;
                 }*/
-                double dens_wall;
-                AOS_prim prim;
                 if(base->problem_number == 1)
-                    dens_wall = SHOCK_TUBE_UL.u1;
-                else {
+                {
+                    // constant left state, the mirror image of SHOCK_TUBE_UR
+                    // in apply_boundary_right. SHOCK_TUBE_UL is already the conserved left state
+                    // built by the same eos at init, so no round-trip through the primitives is needed here
+                    u[i] = SHOCK_TUBE_UL;
+                }
+                else
+                {
+                    AOS_prim prim;
                     prim.density = BACKGROUND_U.u1;
                     if(this->prim[2].speed > 0)
                         prim.speed   = this->prim[2].speed;
                     else
                         prim.speed   = 0.;
                     prim.temperature = const_T_space;
+
+                    eos->update_eint_from_T(&(prim), 1);
+                    eos->update_p_from_eint(&(prim), 1);
+                    eos->compute_conserved(&(prim), &(u[i]), 1) ; //Ghost cell u is fixed after init, only need to update p in prim
                 }
-                
-                eos->update_eint_from_T(&(prim), 1);
-                eos->update_p_from_eint(&(prim), 1);
-                eos->compute_conserved(&(prim), &(u[i]), 1) ; //Ghost cell u is fixed after init, only need to update p in prim
             }
             
             break;